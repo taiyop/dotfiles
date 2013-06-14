570ff1019e03ef0cc78d58e24f35c8a397fb833b
TAIYO AKASHI <taiyop@HANSHIN-no-MacBook-Pro.local>
2013-06-14 12:00:57 +0900

    dotfilesをセットアップするためのシェルスクリプトファイル追加

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# DOT_FILES=( .zsh .zshrc .zshrc.custom .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vim .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc .pryrc)
+
+DOT_FILES=( .zshrc .vimrc .vim
+for file in ${DOT_FILES[@]}
+do
+    rm $HOME/$file
+    ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+# install oh-my-zsh
+[! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
