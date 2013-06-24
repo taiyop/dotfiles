7cd08f109f0a251bba3c6422dc4a8e01f13dc4b9
Taiyo Akashi <onibakidd@gmail.com>
2013-06-24 09:42:43 +0900

    gitconfigの設定を追加

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # DOT_FILES=( .zsh .zshrc .zshrc.custom .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vim .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc .pryrc)
 
-DOT_FILES=( .zshrc .vim .vimrc)
+DOT_FILES=( .zshrc .vim .vimrc .gitconfig)
 for file in ${DOT_FILES[@]}
 do
     rm $HOME/$file
